66007912b70e3c3d15a4bea9580827669d12e61a
Nithin-anim <nithin@animaker.com>
2021-03-30 17:50:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const getAccessBtn = document.getElementById('get-feed-btn');
+const videoEle = document.getElementById('video-feed');
+const canvasEle = document.getElementById('segmented-feed');
+
+const personContext = canvasEle.getContext('2d');
+
+let camAnimation;
+let net;
+
+
+getAccessBtn.addEventListener('click', () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(stream => {
+            videoEle.srcObject = stream;
+            videoEle.play();
+            initBodyPix();
+            videoEle.addEventListener('loadedmetadata', () => {
+                startSegmentation();
+            })
+        });
+})
+
+const initBodyPix = () => {
+    bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 1.0,
+        quantBytes: 2
+    })
+        .then(netObject => {
+            net = netObject;
+        })
+        .catch(err => {
+            console.log('Error', err);
+        });
+}
+
+const drawPerson = (personSegmentation) => {
+    personContext.drawImage(videoEle, 0, 0, 250, 250);
+    const imageData = personContext.getImageData(0, 0, 250, 250);
+    var pixel = imageData.data;
+    for (var p = 0; p < pixel.length; p += 4) {
+        if (personSegmentation.data[p / 4] === 0) {
+            pixel[p + 3] = 0;
+        }
+    }
+    personContext.imageSmoothingEnabled = true;
+    personContext.putImageData(imageData, 0, 0);
+}
+
+const startSegmentation = () => {
+    net.segmentPerson(videoEle, {
+        flipHorizontal: true,
+        internalResolution: 'medium',
+        segmentationThreshold: 0.5
+    })
+        .then(segmentationData => {
+            // console.log("Segmentation Data", segmentationData);
+            drawPerson(segmentationData);
+        })
+        .catch(err => {
+            console.log("Segmentation error", err);
+        })
+    cameraAnimation = requestAnimationFrame(startSegmentation);
+}
